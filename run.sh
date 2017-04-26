6691793a50cfe12d5701da7f115131d5cce42b54
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-04-26 13:29:26 +0200

    Quick and dirty helper runner script.

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Helper script for development. You should probably not run InfiniTAM using
+# this in a serious setting.
+
+dir=$1
+echo "Using dataset directory: ${dir}"
+
+cd build
+make -j4 && \
+./InfiniTAM "${dir}/calib.txt" "${dir}/Frames/%04i.ppm" "${dir}/Frames/%04i.pgm"
+
