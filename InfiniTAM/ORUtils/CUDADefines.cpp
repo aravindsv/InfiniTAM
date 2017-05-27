24896f925d6042960fbd629eca5b4f084f5aa370
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-27 13:35:03 +0200

    Minor cleanup in CUDADefines.cpp

--- InfiniTAM/ORUtils/CUDADefines.cpp
+++ InfiniTAM/ORUtils/CUDADefines.cpp
@@ -7,34 +7,35 @@
 namespace ORUtils {
 
 void __cudaSafeCall(cudaError err, const char *file, const int line) {
-		if (cudaSuccess != err) {
+	if (cudaSuccess != err) {
 
 #ifdef WITH_BACKWARDS_CPP
-				using namespace backward;
-				const int kStackTraceDepth = 32;
-				// The number of top entries to skip when printing the stack trace.
-        // These include two internal layers from `backward-cpp`, plus this current function.
-				const int kStackTraceSkip  = 3;
+		using namespace backward;
+		const int kStackTraceDepth = 32;
+		// The number of top entries to skip when printing the stack trace.
+		// These include two internal layers from `backward-cpp`, plus this current function.
+		const int kStackTraceSkip = 3;
 
-				fprintf(stderr, "\nCUDA error. See stacktrace and details below:\n\n");
+		fprintf(stderr, "\nCUDA error. See stacktrace and details below:\n\n");
 
-				// Display a helpful backtrace (with code snippets, if available).
-				StackTrace st;
-				st.load_here(kStackTraceDepth);
+		// Display a helpful backtrace (with code snippets, if available).
+		StackTrace st;
+		st.load_here(kStackTraceDepth);
 
-				// Disable printing out boilerplate stack frames from the stack trace
-				// processing code.
-				st.skip_n_firsts(kStackTraceSkip);
-				Printer p;
-				p.address = true;
-				p.print(st);
-        fprintf(stderr, "\n");
+		// Disable printing out boilerplate stack frames from the stack trace
+		// processing code.
+		st.skip_n_firsts(kStackTraceSkip);
+		Printer p;
+		p.address = true;
+		p.print(st);
+		fprintf(stderr, "\n");
 #endif
 
-				fprintf(stderr, "%s(%i) : cudaSafeCall() Runtime API error : %s.\n",
-								file, line, cudaGetErrorString(err));
+		fprintf(stderr, "%s(%i) : cudaSafeCall() Runtime API error : %s.\n",
+				file, line, cudaGetErrorString(err));
 
-				exit(-1);
-		}
+		exit(-1);
+	}
 }
+
 }
