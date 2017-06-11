9900ef721945cf3629e3ace3bcccc2d1eb04cd84
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-11 19:04:08 +0200

    Ground truth tracker now uses relative poses

--- InfiniTAM/ITMLib/Engine/ITMGroundTruthTracker.h
+++ InfiniTAM/ITMLib/Engine/ITMGroundTruthTracker.h
@@ -28,20 +28,42 @@ namespace ITMLib {
 	class ITMGroundTruthTracker : public ITMTracker {
 
 	private:
-	  int currentFrame;
-	  vector<Matrix4f> groundTruthPoses;
+		int currentFrame;
+		vector<Matrix4f> groundTruthPoses;
+		Matrix4f currentPose;
+
+		Matrix4f readPose(istream &in) {
+			Matrix4f pose;
+			in >> pose.m00 >> pose.m10 >> pose.m20 >> pose.m30
+				>> pose.m01 >> pose.m11 >> pose.m21 >> pose.m31
+				>> pose.m02 >> pose.m12 >> pose.m22 >> pose.m32;
+			pose.m03 = pose.m13 = pose.m23 = 0.0f;
+			pose.m33 = 1.0f;
+
+			return pose;
+		}
 
 	  // TODO(andrei): Move this helper out of here.
 		/// \brief Loads a KITTI-odometry ground truth pose file.
-		/// \return A list of absolute vehicle poses expressed as 4x4 matrices.
+		/// \return A list of relative vehicle poses expressed as 4x4 matrices.
 	  vector<Matrix4f> readKittiOdometryPoses(const std::string &fpath) {
 		ifstream fin(fpath.c_str());
 		  if (! fin.is_open()) {
 		  throw runtime_error("Could not open odometry file.");
 		}
 
-		  cout << "Loading odometry ground truth from file: " << fpath << endl;
+		cout << "Loading odometry ground truth from file: " << fpath << endl;
+
+		// Keep track of relative transforms, which makes it easier to start reconstructing from an
+		// arbitrary frame, among other things.
 		vector<Matrix4f> poses;
+		Matrix4f first = readPose(fin);
+		Matrix4f inv_prev;
+		first.inv(inv_prev);
+		Matrix4f eye;
+		eye.setIdentity();
+		poses.push_back(eye);
+
 		while (! fin.eof()) {
 			// This matrix takes a point in the ith coordinate system, and projects it
 			// into the first (=0th, or world) coordinate system.
@@ -50,14 +72,10 @@ namespace ITMLib {
 			// the world coordinates, to camera coordinates.
 			//
 			// One therefore needs to set this pose as 'InvM' in the InfiniTAM tracker state.
-
-		  Matrix4f pose;
-		  fin >> pose.m00 >> pose.m10 >> pose.m20 >> pose.m30
-			  >> pose.m01 >> pose.m11 >> pose.m21 >> pose.m31
-			  >> pose.m02 >> pose.m12 >> pose.m22 >> pose.m32;
-			pose.m03 = pose.m13 = pose.m23 = 0.0f;
-		  pose.m33 = 1.0f;
-			poses.push_back(pose);
+			Matrix4f new_pose = readPose(fin);
+			Matrix4f rel_pose = inv_prev * new_pose;
+			new_pose.inv(inv_prev);
+			poses.push_back(rel_pose);
 		}
 
 		return poses;
@@ -81,19 +99,23 @@ namespace ITMLib {
 				 << groundTruthFpath << endl;
 			groundTruthPoses = readKittiOdometryPoses(groundTruthFpath);
 			this->currentFrame = frameOffset;
+			this->currentPose.setIdentity();
 
 			// TODO(andrei): This code, although untested, should provide a skeleton for
 			// reading OxTS data, such that ground truth from the full KITTI dataset can
 			// be read.
 //        vector<OxTSFrame> groundTruthFrames = Objects::readOxtsliteData(groundTruthFpath);
 //        groundTruthPoses = Objects::oxtsToPoses(groundTruthFrames, groundTruthTrans, groundTruthRots);
-	  }
+		}
 
 		void TrackCamera(ITMTrackingState *trackingState, const ITMView *view) {
-			Matrix4f M = groundTruthPoses[currentFrame];
-			trackingState->pose_d->SetInvM(M);
+			// Start with this, since tracking never happens for the first frame (since there's
+			// nothing to track).
 			this->currentFrame++;
-	  }
+			Matrix4f Minc = groundTruthPoses[currentFrame];
+			currentPose = currentPose * Minc;
+			trackingState->pose_d->SetInvM(currentPose);
+		}
 
 	  // Note: this doesn't seem to get used much in InfiniTAM. It's just
 	  // called from 'ITMMainEngine', but none of its implementations
